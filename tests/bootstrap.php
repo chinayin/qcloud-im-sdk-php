5772beb2e30913e1fc936f87ee66f3524c491002
lei.tian <whereismoney@qq.com>
2020-07-10 11:58:23 +0800

    release 0.1

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,3 @@
 <?php
 
-use QcloudIM\App;
-
 require_once __DIR__ . '/../vendor/autoload.php';
-//$app = new App(
-//    [
-//        'corp_id' => 'xxxxxxxxxxxxxxxxxx',
-//        'secret' => 'fsAQmTHCGvd5ZsRJnkr9jj9aCR2sqOKypAUw6D3Jy5Y',
-//        'agent_id' => 0,
-//        'token' => 'cxvhat5gfJW30J9YUsq',
-//        'aes_key' => 'ULPUgqKDC8LTI1fRJ1jOxHrwbUV2QxR5jnSKrlh4meT',
-//        'cache' => [
-//            'path' => __DIR__ . '/runtime/cache',
-//        ],
-//        'log' => [
-//            'file' => __DIR__ . '/runtime/log/app.log',
-//            'level' => 'debug',
-//        ],
-//    ]
-//);
