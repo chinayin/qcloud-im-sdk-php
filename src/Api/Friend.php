f60a7c4752095d4b6d3f91c0530baafdb7c30b1c
lei.tian <whereismoney@qq.com>
2020-07-16 17:39:00 +0800

    调整文案错误

--- src/Api/Friend.php
+++ src/Api/Friend.php
@@ -18,10 +18,10 @@ class Friend
      *
      * @param string        $fromAccountId
      * @param AddFriendItem $item
-     * @param bool          $forceAddFlags 加好友方式（默认双向加好友方式）：
+     * @param bool          $forceAddFlags 管理员强制加好友标记：1表示强制加好友，0表示常规加好友方式
+     * @param string        $addType       加好友方式（默认双向加好友方式）：
      *                                     Add_Type_Single 表示单向加好友
      *                                     Add_Type_Both 表示双向加好友
-     * @param string        $addType       管理员强制加好友标记：1表示强制加好友，0表示常规加好友方式
      *
      * @return array
      */
